b1c8649b8a683c8e5ce3c3976a0c09afbfb3b646
Nayoung-Oh <lightsalt540@gmail.com>
2021-05-12 01:52:55 +0900

    add chart.js

---
+++ chart.js
@@ -0,0 +1,56 @@
+//import { select, json, tree, hierarchy, linkHorizontal } from 'd3';
+
+var treeData = {
+  "name": "경희대학교",
+  "children": [
+      {
+          "name": "서울과학기술대학교",
+          "children":[
+              {
+                  "name": "부천대학교"
+              }
+          ]
+      },
+      {
+          "name": "아주대학교",
+          "children":[
+              {
+                  "name": "인하대학교",
+                  "children":[
+                      {
+                          "name": "경북대학교"
+                      }
+                  ]
+              }
+          ]
+      }
+  ]
+}
+$( document ).ready(function() {
+
+  const width = document.body.clientWidth;
+  const height = document.body.clientHeight;
+
+  const svg = d3.select('div')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+  const treeLayout = d3.tree().size([height, width]); // (*)
+
+  d3.json('./Log2.json').then(data => {
+    const root = d3.hierarchy(data);  // (*)
+    const links = treeLayout(root).links();  // (*)
+    const linkPathGenerator = d3.linkHorizontal()  // (*)
+      .x(d => d.y)
+      .y(d => d.x);
+
+    svg
+      .selectAll('path')
+      .data(links)
+      .enter()
+      .append('path')
+      .attr('d', linkPathGenerator);
+  });
+  
+});
